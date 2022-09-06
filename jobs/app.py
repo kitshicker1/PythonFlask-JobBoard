ff7701ab7105a960faf721a982687eaf7ffd8a3e
kitshicker1 <102931778+kitshicker1@users.noreply.github.com>
2022-09-06 08:55:09 -0500

    9/6

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,14 @@
 from msilib.schema import Class
-from flask import Flask, render_template
+from multiprocessing import connection
+from flask import Flask, render_template, g
+import sqlite3
 
-
+PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
+def open_connect():
+    getattr(g.'_connection') = None
+    return getattr(connection)
 @app.route('/')
 @app.route('/jobs')
 def jobs():
